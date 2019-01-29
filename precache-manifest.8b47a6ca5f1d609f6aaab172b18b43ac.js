a87a98d007cfc15e40e09bd50c4a84b83f11218c
Eduardo Gil <bushiiko@gmail.com>
2019-01-29 14:49:49 -0600

    Updates

---
+++ precache-manifest.8b47a6ca5f1d609f6aaab172b18b43ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "020f0368fc0daf061f7b",
+    "url": "/static/js/main.020f0368.chunk.js"
+  },
+  {
+    "revision": "95604fd842f90e2354fe",
+    "url": "/static/js/1.95604fd8.chunk.js"
+  },
+  {
+    "revision": "020f0368fc0daf061f7b",
+    "url": "/static/css/main.dbe92878.chunk.css"
+  },
+  {
+    "revision": "01ba6c67f57ed918e4f647c38e06adb4",
+    "url": "/index.html"
+  }
+];
