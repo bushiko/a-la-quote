692f00a87cbb81491c9064bc9b511fccc1f4162f
Eduardo Gil <bushiiko@gmail.com>
2019-01-29 14:33:36 -0600

    Updates

---
+++ precache-manifest.86f602287f535371eb8788ab46064b4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "290426111dfe914bd06e",
+    "url": "/a-la-quote/static/js/runtime~main.29042611.js"
+  },
+  {
+    "revision": "020f0368fc0daf061f7b",
+    "url": "/a-la-quote/static/js/main.020f0368.chunk.js"
+  },
+  {
+    "revision": "95604fd842f90e2354fe",
+    "url": "/a-la-quote/static/js/1.95604fd8.chunk.js"
+  },
+  {
+    "revision": "020f0368fc0daf061f7b",
+    "url": "/a-la-quote/static/css/main.dbe92878.chunk.css"
+  },
+  {
+    "revision": "f20962aceab89f6f083058c7a004888f",
+    "url": "/a-la-quote/index.html"
+  }
+];
